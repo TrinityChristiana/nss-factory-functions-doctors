46088d2ef0211a46345bf5bc7836a2860b7b61a6
Trinity Terry <trinitycterry@gmail.com>
2020-01-21 14:27:32 -0600

    finished excercise

--- script.js
+++ script.js
@@ -6,12 +6,31 @@
 
 const createDoctor = (name, specialty, address) => {
     return {
-        "name": name,
-        "specialty":  specialty,
-        "address": address
+        name: name,
+        specialty: specialty,
+        address: address
     };
 };
 
 console.log("Created Doctor", createDoctor("John Appleseed", "Oncologist", "301 Plus Park"));
 
+/* 
+    Lightning Exercise 2: Write a factory function that creates an object that represents a pet. The function should accept two arguments.
 
+    1. Pet name
+    2. Pet breed
+    3. Invoke the factory function 3 times and place each animal in an array stored in a variable named BowWowKennels 
+*/
+
+const createPet = (name, breed) => {
+    return {
+        name: name,
+        breed: breed
+    };
+};
+
+let bowWowKennels = [];
+bowWowKennels.push(createPet("Luna", "Hava Cavapoo"));
+bowWowKennels.push(createPet("Skeeter", "Hava Cavapoo"));
+bowWowKennels.push(createPet("Diego", "Labrador Retriever/American Staffordshire Terrier"));
+console.log(bowWowKennels);
